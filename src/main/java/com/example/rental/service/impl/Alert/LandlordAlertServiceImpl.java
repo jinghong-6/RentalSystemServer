d4a9c0ba5b9fe591b53b4848c60603a8cd23334d
jinghong-6 <139053090+jinghong-6@users.noreply.github.com>
2024-01-09 22:37:53 +0800

    补充商家通知接口

--- src/main/java/com/example/rental/service/impl/Alert/LandlordAlertServiceImpl.java
+++ src/main/java/com/example/rental/service/impl/Alert/LandlordAlertServiceImpl.java
@@ -1,28 +1,73 @@
 package com.example.rental.service.impl.Alert;
 
+import com.example.rental.dao.Alert.LandlordAlertDao;
+import com.example.rental.dao.HouseDao;
+import com.example.rental.dao.Order.OrderCompleteDao;
+import com.example.rental.dao.Order.OrderCompletedDao;
+import com.example.rental.dao.Order.OrderNopayDao;
 import com.example.rental.service.Alert.LandlordAlertService;
+import com.example.rental.utils.Code;
 import com.example.rental.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class LandlordAlertServiceImpl implements LandlordAlertService {
+    @Autowired
+    LandlordAlertDao landlordAlertDao;
+
+    @Autowired
+    OrderNopayDao orderNopayDao;
+
+    @Autowired
+    HouseDao houseDao;
+
+    @Autowired
+    OrderCompleteDao orderCompleteDao;
+
+    @Autowired
+    OrderCompletedDao orderCompletedDao;
+
     @Override
     public Result getAlertByLandlordId(String LandlordId) {
-        return null;
+        List<Map<String, String>> AlertList = landlordAlertDao.getAlertListByLandlordId(LandlordId);
+        if (AlertList.size() == 0) {
+            return new Result(Code.SEARCH_ERR, "暂无通知");
+        } else {
+            return new Result(Code.SEARCH_OK, AlertList);
+        }
     }
 
     @Override
     public Result getAlertByLandlordIdAndAlertId(String LandlordId, String AlertId) {
-        return null;
+        Map<String, String> Alert = landlordAlertDao.getAlertByLandlordIdAndAlertId(LandlordId, AlertId);
+        if (Alert != null) {
+            return new Result(Code.SEARCH_OK, Alert);
+        } else {
+            return new Result(Code.SEARCH_ERR, "未查询到");
+        }
     }
 
     @Override
     public Result updateLandlordAlertStatus(String LandlordId, String AlertId) {
-        return null;
+        Map<String, String> Alert = landlordAlertDao.getAlertByLandlordIdAndAlertId(LandlordId, AlertId);
+        if (Alert != null) {
+            return new Result(Code.SEARCH_OK, Alert);
+        } else {
+            return new Result(Code.SEARCH_ERR, "未查询到");
+        }
     }
 
     @Override
     public Result getAlertCountByLandlordId(String LandlordId) {
-        return null;
+        Integer num = landlordAlertDao.getAlertCountByLandlordId(LandlordId);
+        if (num != null) {
+            return new Result(Code.SEARCH_OK, num);
+        } else {
+            return new Result(Code.SEARCH_ERR, "查询失败");
+        }
     }
 }
